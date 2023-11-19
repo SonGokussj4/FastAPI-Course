9eb578edf6846a73c471b7876e96f4d1d4430c36
SonGokussj4 <SonGokussj4@centrum.cz>
2023-11-19 23:18:40 +0100

    [6:10:32] Hash user password

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ from fastapi import Depends, FastAPI, HTTPException, Response, status
 from psycopg2.extras import RealDictCursor  # Return a col_name as a key in a dict
 from sqlalchemy.orm import Session
 
-from . import models, schemas
+from . import models, schemas, utils
 from .database import engine, get_db
 
 models.Base.metadata.create_all(bind=engine)
@@ -111,6 +111,10 @@ def delete_post(id: int, db: Session = Depends(get_db)):
 
 @app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    # Hash the password - user.password
+    hashed_password = utils.hash(user.password)
+    user.password = hashed_password
+
     new_user = models.User(**user.model_dump())
     db.add(new_user)
     db.commit()
