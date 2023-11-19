61175ac433b2ded73e254bc3f578e0ea16b35d12
SonGokussj4 <SonGokussj4@centrum.cz>
2023-11-19 19:30:51 +0100

    [5:01:00] Add 'created_at' column to models with server_default text now()

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy.sql.expression import text
+from sqlalchemy.sql.sqltypes import TIMESTAMP
 
 from .database import Base
 
@@ -10,3 +12,4 @@ class Post(Base):
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
     published = Column(Boolean, server_default="TRUE", nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), server_default=text("now()"), nullable=False)
