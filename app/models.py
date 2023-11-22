db2183effa37db5d9165825b74c09cada08df479
SonGokussj4 <SonGokussj4@centrum.cz>
2023-11-22 02:13:59 +0100

    [9:34:20] Create 'votes' table with Combined primary keys for USERS.id and POSTS.id

--- app/models.py
+++ app/models.py
@@ -28,3 +28,10 @@ class User(Base):
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), server_default=text("now()"), nullable=False)
+
+
+class Vote(Base):
+    __tablename__ = "votes"
+
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), primary_key=True, nullable=False)
+    post_id = Column(Integer, ForeignKey("posts.id", ondelete="CASCADE"), primary_key=True, nullable=False)
